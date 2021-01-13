fb0697c6dde3f9ce2edd25a6e05f04013ada757c
Tony Yang <txy3@cornell.edu>
2021-01-12 22:07:16 -0500

    basic auth stuff

--- frontend/src/Authentication.tsx
+++ frontend/src/Authentication.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import 'firebase/auth';
+import Login from './Login';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import firebase from 'firebase/app';
-import { useEffect } from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+//import { useEffect } from 'react';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlnpgB1moM8CrTWNJSjKP2abpCA0ZGIo8",
@@ -25,9 +27,89 @@ type User = {
 }
 
 const Authentication = () => {
+
+  const [user, setUser] = useState<User | null>(null);
+  const [message, setMessage] = useState('');
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+
+
+  const signup = (email: string, password: string, firstName: string, lastName: string) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCreds) => {
+        if (userCreds !== null) {
+          const user = userCreds.user;
+          const uid = user?.uid;
+          axios.post('/createUser', { uid, firstName, lastName })
+            .then(async (res) => {
+              const user = await axios.get<User>(`/getUser?uid=${uid}`);
+              setUser(user.data);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log(error.code);
+        switch (error.code) {
+          case ('auth/email-already-in-use'):
+            setMessage('Account with this email address already exists!');
+            break;
+          case ('auth/invalid-email'):
+            setMessage('Please enter a valid email!');
+            break;
+          default:
+            setMessage('Sign up unsuccessful!');
+        }
+        setSnackBarOpen(true);
+      });
+  }
+
+  const login = (email: string, password: string) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(async (userCreds) => {
+        if (userCreds !== null) {
+          const uid = userCreds.user?.uid;
+          const user = await axios.get<User>(`/getUser?uid=${uid}`);
+          setUser(user.data);
+        }
+
+      })
+      .catch((error) => {
+        console.log(error.message);
+        switch (error.code) {
+          case ('auth/invalid-email'):
+            setMessage('Please enter a valid email!');
+            break;
+          case ('auth/user-not-found'):
+            setMessage('Invalid email or password!');
+            break;
+          case ('auth/wrong-password'):
+            setMessage('Invalid email or password!');
+            break;
+          default:
+            setMessage('Log in unsuccessful!');
+        }
+        setSnackBarOpen(true);
+      });
+  }
+
   return (
     <div>
-
+      <Switch>
+        <Route path="/signup">
+          <Redirect to="/" />
+        </Route>
+        <Route path="/">
+          <Login callback={login} />
+        </Route>
+      </Switch>
+      <Snackbar
+        message={message}
+        open={snackBarOpen}
+        autoHideDuration={5000}
+        onClose={() => setSnackBarOpen(false)}
+      />
     </div>
   )
 }
