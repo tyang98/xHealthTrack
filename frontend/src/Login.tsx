2f571ec99fcbc3f794a8499ef17a720b7495035f
ryanwmao <52467929+ryanwmao@users.noreply.github.com>
2021-01-13 15:15:41 -0500

    login stuff

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -2,25 +2,57 @@ import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import './Login.css';
 
-type LoginProp = {
-    callback: (email: string, password: string) => void;
+type LoginProps = {
+    // callback: (email: string, username: string, password: string) => void;
+    callback: (username: string, password: string) => void;
 }
 
-const Login = ({ callback }: LoginProp) => {
-    const [email, setEmail] = useState('');
+const Login = ({ callback }: LoginProps) => {
+    // const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setEmail(event.target.value);
+    // const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    //     setEmail(event.target.value);
+    // }
+
+    const onChangeUsername = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setUsername(event.target.value);
     }
+
     const onChangePassword = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setPassword(event.target.value);
     }
 
     return (
-        <div>
-            <h1>Ez</h1>
+        <div className="Sections">
+            <h1>Header ?? text here </h1><br/>
+            {/* <TextField
+                label="Email"
+                className="Section"
+                value={email}
+                onChange={onChangeEmail} /> <br/> */}
+            <TextField
+                label="Username"
+                className="Section"
+                type="text"
+                value={username}
+                onChange={onChangeUsername} /> <br/>
+            <TextField
+                label="Password"
+                className="Section"
+                value={password}
+                type="password"
+                onChange={onChangePassword} /> <br/><br/>
+            <Button 
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                    callback(username, password)
+                }}
+            >Login</Button>
         </div>
+        //create acct ?
     );
 }
 
