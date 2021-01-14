cc6576f1f0b797a8bd0eda805cf197735304913e
Tony Yang <txy3@cornell.edu>
2021-01-14 17:53:22 -0500

    finish login/register background

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { TextField, Button, InputAdornment, IconButton} from '@material-ui/core';
 import './Login.css';
+import background from 'frontend/src/images/img.png';
+import ScrollLock from 'react-scrolllock';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'
 
@@ -8,7 +10,6 @@ import VisibilityOffIcon from '@material-ui/icons/VisibilityOff'
 type LoginProps = {
     callback: (email: string, password: string) => void;
 }
-
 
 const Login = ({ callback }: LoginProps) => {
     const [email, setEmail] = useState('');
@@ -26,10 +27,13 @@ const Login = ({ callback }: LoginProps) => {
     }
 
     return (
-        <div className="Background">
-            <div className="Wrapper">
+        <div>
+            <ScrollLock>
+            <img src={background} alt={""} style={{ minHeight: '100%', minWidth: '100%', position: 'fixed', top: '0', left: '0', zIndex: -1 }} />
+            </ScrollLock>
+            <div className="Wrapper" style={{ justifyContent: 'center'}}> 
                 <div className="Sections">
-                    <h1>please change project name</h1><br/>
+                    <h1>xhy0rinstyx</h1><br/>
                     <TextField
                         label="Email"
                         className="Section"
