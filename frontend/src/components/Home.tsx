3dee4548669f706f74fcfbb4f62b37798b0ca63c
ryanwmao <52467929+ryanwmao@users.noreply.github.com>
2021-01-15 14:10:26 -0500

    linked sheets ?

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ScrollLock from 'react-scrolllock';
 import Footer from './Footer';
 import background from 'frontend/src/images/img.png';
+import "bootstrap/dist/css/bootstrap.css";
+
+const Tabletop = require('tabletop');
 
 const Home = () => {
 
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    Tabletop.init({
+        key: '1OsV0V-ffEF4-BkCqhoKXPlaJN__g_B94fZEsUt1cKXU',
+        callback: (data: any) => {
+            setItems(data)
+        },
+        simpleSheet: true
+      })
+  }, [])
+  
   return (
     <div>
       <ScrollLock>
