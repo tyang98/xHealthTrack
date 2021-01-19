40873231ea0e853dd33d448fed29da03bc825232
Tony Yang <txy3@cornell.edu>
2021-01-19 03:21:29 -0500

    update homepage

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ScrollLock from "react-scrolllock";
 import NavigationBar from "./NavigationBar";
+import moment from 'moment';
 import Footer from "./Footer";
+import '../styles/Home.css';
 import background from "frontend/src/images/img.png";
 import { Button, Modal } from "react-bootstrap";
+import axios from 'axios';
+import 'firebase/auth';
+import firebase from 'firebase/app';
 import Check from "./Check";
 import WChart from "./WChart";
 
@@ -12,11 +17,30 @@ type HomeProps = {
   callback: () => void;
 };
 
+type User = {
+  uid: string;
+  firstName: string;
+  lastName: string;
+}
+
 const Home = ({ callback }: HomeProps) => {
+  const [firstName, setFirstName] = useState('br');
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const getWelcome = () => {
+    const firebaseUser = firebase.auth().currentUser;
+    const uid = firebaseUser?.uid;
+    axios.get<User>(`/getUser?uid=${uid}`)
+    .then((user) => { setFirstName(user.data.firstName)})
+    .catch((error) => console.log(error));
+  }
+
+  useEffect(() => getWelcome(), []);
+
+  const welcomeMessage = `Welcome ${firstName}! `;
 
   return (
     <div className="home">
@@ -38,7 +62,10 @@ const Home = ({ callback }: HomeProps) => {
       {/* daily check */}
       <div className="container-fluid pt-3">
         <div className="row">
+          
           <div className="col">
+            <h1>{welcomeMessage} <span className="wave">👋</span> It is {moment().format(' h:mm:ss a, MMMM Do YYYY')}</h1>
+            <br/>
             <div className="card text-left text-primary bg-light border-primary">
               <div className="d-flex align-items-center">
                 <div className="mr-auto p-2">
