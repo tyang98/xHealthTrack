6444f959defb236d51090c92be2c3c16ba540cc3
Tony Yang <txy3@cornell.edu>
2021-01-27 02:40:28 -0500

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -19,8 +19,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-const port = 8080;
-//const port = 8080;
+
+const port = process.env.PORT || 8080;
 
 
 type Activity = {
